340090a0229386cd75e20a75059df6c93a8c5e6e
NiaNyaa <109681800+NiaNyaa@users.noreply.github.com>
2023-07-09 17:35:09 +0700

    Create cpp_jenis_variabel.cpp

---
+++ cpp_jenis_variabel.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int angka;
+  string huruf;
+  float desimal;
+  bool boolean;
+  cout << "masukkan integer:" ;
+  cin >> angka;
+  cout << "masukkan string:" ;
+  cin >> huruf;
+  cout << "masukkan float:" ;
+  cin >> desimal;
+  cout << "masukkan boolean (1 atau 0):";
+  cin >> boolean;
+  cout << "integer yang dimasukkan: " << angka << endl;
+  cout << "string yang dimasukkan: " << huruf << endl;
+  cout << "float yang dimasukkan: " << desimal << endl;
+  cout << "boolean yang dimasukkan: " << boolean << endl;
+  
+}
