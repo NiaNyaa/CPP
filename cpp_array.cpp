16b033bbb40e36a62e2ed489948373d0af721c76
NiaNyaa <109681800+NiaNyaa@users.noreply.github.com>
2023-07-09 17:39:25 +0700

    Create cpp_array.cpp

---
+++ cpp_array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int myList[10];
+    
+    myList[0] = 10;
+    myList[1] = 20;
+    myList[2] = 30;
+    cout << "elemen " << myList[0] << endl;
+    cout << "elemen " << myList[1] << endl;
+    cout << "elemen " << myList[2] << endl;
+
+    return 0;
+}
