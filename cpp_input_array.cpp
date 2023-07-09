1b6846e7021836ee24cb9c6606ecad1db79ea0f2
NiaNyaa <109681800+NiaNyaa@users.noreply.github.com>
2023-07-09 17:41:13 +0700

    Create cpp_input_array.cpp

---
+++ cpp_input_array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int myList[3];
+
+    cout << "Masukkan elemen 1: ";
+    cin >> myList[0];
+
+    cout << "Masukkan elemen 2: ";
+    cin >> myList[1];
+
+    cout << "Masukkan elemen 3: ";
+    cin >> myList[2];
+
+    cout << "Elemen 1: " << myList[0] << endl;
+    cout << "Elemen 2: " << myList[1] << endl;
+    cout << "Elemen 3: " << myList[2] << endl;
+
+    return 0;
+}
